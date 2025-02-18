1ecfd6b7b315c74559a9ddb3ebdb1d0fd33bcde3
luis <henrique.luis@luizalabs.com>
2025-02-18 16:21:46 -0300

    refactoring stuff

---
+++ LogExtractor.py
@@ -0,0 +1,271 @@
+from actions import ArqManipulation
+import pandas as pd
+import re
+
+class PytestArtifactLogExtractor:
+    """f
+    A class to extract and process test status and timing information from a pytest artifact log.
+    """
+
+    def __init__(self, path: str):
+        """
+        Initializes the PytestArtifactLogExtractor object.
+
+        :param path: Path to the pytest artifact log file.
+        """
+        self.path = path
+        self.data = self.__read_file__()
+
+    def __read_file__(self):
+        """
+        Reads the contents of the log file and returns it as a string.
+
+        :return: String containing the file content.
+        """
+        with open(self.path, "r") as file: 
+            data = file.read()
+
+        return ArqManipulation.clean_ansi_escape(data)
+
+    def log_to_df(self):
+        """
+        Parses the log file to extract test results and performance metrics.
+
+        :return: A DataFrame combining test statuses with time metrics.
+        """
+
+        df_parquet = ArqManipulation.read_parquet_file(parquet_file_name='pytest.log.parquet')   
+
+        databaseId = self.__extract_self_path_info__().get('databaseId').get(0)
+        databaseId = int(databaseId) if databaseId else None 
+    
+        if not df_parquet.empty and (databaseId in df_parquet['databaseId']):
+            return df_parquet
+
+        tests, categories, failures = self.__extract_all_categories__()
+        
+        # Creating dataframes test status and categories
+        status_df = self.__create_status_df__(tests).set_index('name')
+        categories_df = self.__create_time_df__(categories)
+        failures_df = pd.DataFrame(failures, columns=['name', 'category', 'arguments', 'error', 'error_details']).set_index('name')
+
+        # Labeling the dfs
+        status_df.index.name = 'pytest_tests_status'
+        categories_df.index.name = 'pytest_run_times'
+        failures_df.index.name = 'pytest_failures_errors'
+
+        # Applying individual id for each table
+        status_df['databaseId'] = databaseId
+        categories_df['databaseId'] = databaseId
+        failures_df['databaseId'] = databaseId
+
+
+        return status_df, categories_df, failures_df
+
+    def __create_status_df__(self, data):
+        formatted_data = []
+
+        return pd.DataFrame(formatted_data, columns=["status", "name", "category", "arguments"])
+
+    def __extract_all_categories__(self):
+        """
+        Converts extracted timing data into DataFrames.
+
+        :param values: A list of lists with extracted time metrics.
+        :type values: list[list]
+        :return: A list of DataFrames with execution time statistics.
+        :rtype: list[pandas.DataFrame]
+        """
+        header = []
+        # Filtering out irrelevant categories
+        keywords = ('deselected', 'passed in', 'grand total', 'live log')
+
+        # Breaking each file into an list that contains the category as the pos 0
+        values = self.data.splitlines()
+        for value in values:
+            if any(k in value for k in keywords):
+                continue   
+            elif re.match(r'=+|-+', value): # Divide by headers demarked by '=' or '-' (logging)
+                value = value.replace("=", "")  
+                value = value.replace("-", "")  
+                header.append([value]) 
+            else:
+                # Populate each category and break in the case of the pytest-durations tables while ignoring empty values
+                header[-1].append(value)
+
+        headers = [['live_log','live_log','live_log']]
+        # Ignore cases of logging mode is active
+        if not 'live log' in self.data:
+            headers = self.__extract_test_status_names__(self.__get_list_by_name__(header, 'test session')[0])    
+        categories = self.__extract_categories__(self.__get_list_by_name__(header, 'duration top'))
+        failures = self.__extract_failures_errors__(self.__get_list_by_name__(header, 'summary'))
+
+        return headers, categories, failures
+
+    def __extract_categories__(self, data):
+        categories = []
+        for d in data:
+            categories.append([])
+            for s in d: 
+                formatted_s = list(filter(None,s.split(" ")))
+                if 'duration' in formatted_s: #converting the header name back into string
+                    formatted_s = '_'.join(formatted_s)
+                categories[-1].append(formatted_s)
+
+        return categories
+
+
+    def __get_list_by_name__(self, data: list, name: str):
+        """
+        Find the sublist containing the specified name in the first element.
+
+        :param data: A list of sublists to search through.
+        :type data: list[list]
+        :param name: The name to search for in the first element of each sublist.
+        :type name: str
+        :return: A list of sublists where the first element matches the name.
+        :rtype: list[list]
+        """
+        matching_sublists = []
+        
+        for sublist in data:
+            if re.search(name, sublist[0]):  # Converte os itens para string
+                matching_sublists.append(sublist)
+        
+        return matching_sublists
+
+    def __extract_test_status_names__(self, data):
+        """
+        Extracts the status and the tests names out of the pytest log, breaking them down to a list of lists.
+
+        :param data: A list of lines containing test results.
+        :type data: list[str]
+        :return: list[list[str]]: A list of lists with test names, statuses (PASSED, FAILED, ERROR), and additional details.
+        """
+
+        tests = []
+        keywords = ('PASSED', 'FAILED', 'ERROR', 'SKIPPED')
+
+        for line in data:
+            if any(k in line for k in keywords):
+                match = re.search(r'(PASSED|FAILED|ERROR).*', line).group()
+                # Splitting the Keyword NameTest from category and argument
+                match = re.split(r'::', match, 1)
+                tmp = re.split('\s', match[0], maxsplit=1)
+                # Splitting the category from arguments
+                tmp += re.split(r'\[', match[1], maxsplit=1)
+                # Allow degenerated data to fit in the dataframe
+                while(len(tmp) < 4):
+                    tmp.append(None)
+
+                tests.append(tmp)
+
+        return tests
+
+    def __extract_failures_errors__(self, data):
+
+        """
+        Extracts from the pytest log the details of tests with failures or errors cleaning the data to make it ready to a dataframe.
+
+        :param data: A list of strings containing test results.
+        :type data: list[str]
+        :return: list[list]: A list of lists containing details of tests with failures and/or errors.
+        """
+        # Some test wont have errors, but there still need a dataframe
+        if not data:
+            return [[None]*5]
+        data = data[0]
+
+        print(data)
+
+        # Regex asks for a string, cleaning it and concatening the list
+        data_str = ''.join(list(''.join(d) for d in data[1:]))
+        data_str = list(filter(None, re.split(r'(FAILED|ERROR)', data_str)))
+
+        splitted_data = []
+
+        # Splitting test from error
+        for d in data_str:
+            if d and ('FAILED' or 'ERROR') not in d:
+                splitted_data.append(list(filter(None, re.split(r'\[(.*?)\]-|::|(\w+):([\w=*]+)', d))))
+
+        cleaned_splitted_data = []
+
+        for s in splitted_data:
+            if len(s) > 5:
+                    s[5] = " ".join(s[5:])
+            cleaned_splitted_data.append(s[:5])
+
+        return cleaned_splitted_data
+
+    def __create_time_df__(self, values):
+        """
+        Converts extracted timing information into DataFrames.
+
+        :param values: A list of lists containing extracted time metrics.
+        :return: A list of DataFrames with execution time statistics.
+        """
+        dfs = pd.DataFrame()
+        
+        for h in values:
+            time_df = pd.DataFrame(h[2:], columns=h[1])
+
+            # Converting time-related columns to datetime.time format
+            time_columns = ['avg', 'min', 'total']
+            for col in time_columns:
+                if col in time_df.columns:
+                    time_df[col] = pd.to_timedelta(time_df[col], errors='coerce').dt.total_seconds().round(3)
+                    
+            # Assigning a 'durationType' column for metric categorization
+            time_df['durationType'] = h[0].replace('top', '').replace('test', '')
+
+            dfs = pd.concat([time_df, dfs], ignore_index=True)
+
+        if 'name' in dfs.columns:
+            dfs = dfs.set_index('name')
+
+        return dfs
+
+    def __extract_self_path_info__(self):
+        """
+        Extracts test and database ID information from the log file path.
+
+        :return: A DataFrame containing 'test' and 'databaseId' information.
+        """
+        # Extract filename without extension
+        stripped = self.path.split('/')[-1].split('.')
+        stripped.pop()  # Remove the file extension
+
+        # Ensure there are exactly three elements (fill missing ones with None)
+        while len(stripped) < 3:
+            stripped.append(None)  # Fill missing values with NaN
+
+        # Create DataFrame
+        df = pd.DataFrame([stripped], columns=['test', 'region', 'databaseId'])
+
+        return df
+
+    def __merge_artifact_dfs__(self, times_df, status_df):
+        """
+        Merges test execution time data with test status information.
+
+        :param times_df: A list of DataFrames containing time-related data.
+        :param status_df: A DataFrame containing test statuses.
+        :return: A combined DataFrame containing execution metrics and test results.
+        """
+        databaseId_df = self.__extract_self_path_info__()  
+        order = ['category', 'durationType', 'databaseId', 'status', 'num', 'avg', 'min', 'total']
+        dfs = []
+
+        for h in times_df:
+            joined_df = h.join(status_df)  # Merging time metrics with test statuses
+
+            # Adding database ID to each row
+            for col in databaseId_df.columns.values:
+                joined_df[col] = databaseId_df[col].values[0]  
+
+            # Reordering columns
+            joined_df = joined_df[order]  
+            dfs.append(joined_df)
+
+        return pd.concat(dfs)  
---
+++ actions.py
@@ -0,0 +1,362 @@
+import subprocess
+import os
+import shutil
+import pandas as pd
+import subprocess
+import re
+import json
+
+
+class ArqManipulation:
+    """
+    A utility class for file operations and data manipulation.
+    """
+
+    @staticmethod 
+    def read_parquet_file(parquet_file_name: str) -> pd.DataFrame:
+        """
+        Reads a Parquet file and returns a DataFrame.
+
+        :param parquet_file_name: Path to the Parquet file.
+        :return: DataFrame with file contents.
+        """
+        try:
+            if not os.path.exists(parquet_file_name):
+                #print(f"File '{parquet_file_name}' does not exist.")
+                return pd.DataFrame()
+            
+            return pd.read_parquet(parquet_file_name)
+        except Exception as e:
+            raise RuntimeError(f"Error reading Parquet file '{parquet_file_name}': {e}")
+
+    @staticmethod
+    def save_df_to_parquet(df: pd.DataFrame, parquet_file_name: str):
+        """
+        Saves a DataFrame to a Parquet file.
+
+        :param df: Dataframe to save.
+        :param parquet_file_name: Parqueet saving path.
+        """
+        try:
+            os.makedirs(os.path.dirname(parquet_file_name), exist_ok=True)
+            df.to_parquet(parquet_file_name)
+            print(f"DataFrame successfully saved to {parquet_file_name}")
+        except Exception as e:
+            raise RuntimeError(f"Error saving DataFrame to Parquet file '{parquet_file_name}': {e}")
+
+    @staticmethod
+    def clean_ansi_escape(base_str: str) -> str:
+        """
+        Removes ANSI escape values from a string.
+
+        :param base_str: Unformmated string.
+        :return: Cleaned string.
+        """
+        return re.sub(r'\x1B\[[0-9;]*[A-Za-z]', '', base_str)
+
+    @staticmethod
+    def parse_stdout_json(base_str: str) -> dict:
+        """
+        Parses JSON output from GitHub CLI after cleaning ANSI escape sequences.
+
+        :param base_str: The raw output string from the GitHub CLI.
+        :return: Parsed JSON dictionary.
+        """
+        try:
+            cleaned = ArqManipulation.clean_ansi_escape(base_str)
+            str_output = ''.join(cleaned.splitlines())
+            return json.loads(str_output)
+        except json.JSONDecodeError as e:
+            raise e
+
+    @staticmethod
+    def json_to_df(parsed_json: dict) -> pd.DataFrame:
+        """
+        Converts a JSON dictionary to a sorted DataFrame with specific columns.
+
+        :param parsed_json: Parsed JSON data.
+        :return: Pandas DataFrame sorted by the 'createdAt' column.
+        """
+        try:
+            df_json = pd.DataFrame(parsed_json)
+            required_columns = ['name', 'createdAt', 'conclusion', 'status', 'databaseId', 'workflowDatabaseId']
+            
+            if not all(col in df_json.columns for col in required_columns):
+                raise KeyError(f"Missing required columns in JSON data: {set(required_columns) - set(df_json.columns)}")
+
+            df_json['createdAt'] = pd.to_datetime(df_json['createdAt'])
+            return df_json[required_columns].sort_values(by="createdAt")
+        except KeyError as e:
+            raise ValueError(f"Error processing JSON to DataFrame: {e}")
+        except Exception as e:
+            raise RuntimeError(f"Unexpected error in json_to_df: {e}")
+
+class ActionsArtifacts:
+    """
+    A class to handle downloading, retrieving, and deleting GitHub Actions artifacts.
+    """
+
+    def __init__(self, repository: str):
+        """
+        Initializes the ActionsArtifacts object.
+
+        :param repository: The GitHub repository in the format "owner/repo".
+        """
+        self.repository = repository
+        self.folder = 'artifacts/'  # Default storage dir
+        self.paths = self.retrieve_downloaded_artifacts() 
+
+    def download_artifact(self, database_id: str):
+        """
+        Downloads an artifact from GitHub Actions using the GitHub CLI.
+
+        :param database_id: The database ID of the artifact to download.
+        """
+        try:
+            # Ensure the folder exists before downloading
+            os.makedirs(self.folder, exist_ok=True)
+
+            # Construct the command to download the artifact
+            command = f'gh run --repo {self.repository} download {database_id} --dir {os.path.join(self.folder, str(database_id))}'
+
+            # Execute the command
+            subprocess.run(command, shell=True, text=True, check=True)
+            print("Download Successful")
+        except subprocess.CalledProcessError as e:
+            print(f"Error during artifact download: {e}")
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+
+    def retrieve_downloaded_artifacts(self) -> list[str]:
+        """
+        Retrieves all downloaded artifacts file paths.
+
+        :return: returns Paths of the downloaded artifacts
+        """
+        paths = []
+
+        # Walk through the artifacts folder and collect all file paths
+        for path, _, files in os.walk(self.folder):
+            for file in files:
+                paths.append(os.path.join(path, file))
+
+        return paths
+
+    def delete_downloaded_artifacts(self):
+        """
+        Deletes all downloaded artifacts recursively
+        """
+        try:
+            shutil.rmtree(self.folder)
+            if os.path.exists(self.folder):
+                print("Error: Failed to delete artifacts directory.")
+            else:
+                print("Artifacts directory deleted successfully.")
+        except FileNotFoundError:
+            print("Artifacts directory not found, nothing to delete.")
+        except Exception as e:
+            print(f"Error while deleting artifacts: {e}")
+
+class ActionsWorkflow:
+    """
+    A class to extract GitHub Actions workflows using the GitHub CLI, generating a dataframe with returned data
+    """
+
+    def __init__(self, repository, query_size):
+        """
+        Initializes the ActionsWorkflow class.
+
+        :param repository: GitHub repository in the format "owner/repo".
+        :param query_size: Number of workflows to retrieve.
+        """
+        self.repository = repository
+        self.json_attributes = '--json name,status,conclusion,createdAt,databaseId,workflowDatabaseId'
+        self.query_size = query_size
+        self.df = self.__gh_list_query__()
+
+    def __gh_list_query__(self):
+        """
+        Calls the GitHub API via the GitHub CLI (`gh run list`) and retrieves
+        a specified number of workflows.
+
+        :return: A DataFrame containing the parsed workflow data.
+        """
+        try:
+            list_command = f'gh run --repo {self.repository} list {self.json_attributes} -L {self.query_size}'
+            
+            output_json = subprocess.run(
+                list_command, shell=True, text=True, check=True, capture_output=True
+            ).stdout
+
+            parsed_json = ArqManipulation.parse_stdout_json(output_json)
+            df = ArqManipulation.json_to_df(parsed_json)
+
+            ArqManipulation.save_df_to_parquet(df = df, parquet_file_name="./bin/actionsWorflow.parquet")
+
+            return df.set_index('name')
+
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing GitHub CLI command: {e}")
+            return pd.DataFrame()  # Return an empty DataFrame on error
+
+class ActionsJobs:
+    """
+    A class to interact with GitHub Actions jobs using the GitHub CLI.
+    """
+
+    def __init__(self, repository, workflow):
+        """
+        Initializes the ActionsJobs class.
+
+        :param repository: GitHub repository in the format "owner/repo".
+        :param workflow: Workflow associated with the jobs.
+        """
+        self.repository = repository
+        self.workflow = workflow  
+
+    def __retrieve_jobs__(self, database_id: int):
+        command = f'gh run --repo {self.repository} view {database_id}'
+        jobs_data = subprocess.run(command, shell=True, text=True, check=True, capture_output=True).stdout
+
+        return jobs_data
+
+    def get_jobs(self, database_id: int) -> pd.DataFrame:
+            """
+            Retrieves job data from the GitHub CLI and processes it.
+
+            :param database_id: The ID of the workflow run.
+            :return: A Pandas DataFrame containing job details.
+            """
+            try:
+                jobs_df = ArqManipulation.read_parquet_file(parquet_file_name="./bin/actionsJobs.parquet")
+
+                if jobs_df.empty:
+                    data = self.__retrieve_jobs__(database_id=database_id)
+                    jobs_df = self.__clean_job_text__(data)
+
+                    jobs_df["databaseId"] = int(database_id)
+
+                    ArqManipulation.save_df_to_parquet(jobs_df, parquet_file_name="./bin/actionsJobs.parquet")
+
+                elif not database_id in jobs_df['databaseId'].values:
+                    data = self.__retrieve_jobs__(database_id=database_id)
+                    data_df = self.__clean_job_text__(data)
+                    data_df["databaseId"] = int(database_id)
+
+                    jobs_df = pd.concat([jobs_df, data_df], ignore_index=True)
+
+                    #ArqManipulation.save_df_to_parquet(jobs_df, parquet_file_name="./bin/actionsJobs.parquet")
+
+                return jobs_df
+
+            except subprocess.CalledProcessError as e:
+                print(f"Error executing GitHub CLI command: {e}")
+                return pd.DataFrame()
+
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+                return pd.DataFrame()
+        
+    def __split_string__(self, job_list):
+        """
+        Splits a job string into structured components.
+
+        :param job: The job string to split.
+        :return: A list of cleaned job attributes.
+        """
+        jobs = []
+
+        for job in job_list:
+            delimiters = r" \| | / build in | \(ID |\| in| / cleanup in | /| in " 
+            splitted_job = re.split(delimiters, job)
+            splitted_job = [s.strip() for s in splitted_job if s.strip()]
+            jobs.append(splitted_job)
+        
+        jobs.pop(0)
+
+        return jobs
+
+    def __build_cleaned_df__(self, data):
+        # Define columns
+        columns = ["conclusion", "test", "buildTime (sec)", "jobId"]
+        jobs_df = pd.DataFrame(columns=columns)
+        jobs_df["failedAt"] = None
+
+        for job in data:
+            if any("ID" in item and ("PASSED" in item or "FAILED" in item) for item in job):
+                temp_df = pd.DataFrame(self.__split_string__(job), columns=columns)
+
+                temp_df['buildTime (sec)'] = temp_df['buildTime (sec)'].apply(str_time_to_int)
+                jobs_df = pd.concat([jobs_df, temp_df], ignore_index=True)
+            
+            elif any("FAILED" in item for item in job):
+                failed = next(item for item in job if "FAILED" in item).split("FAILED | ")
+                if not jobs_df.empty:
+                    jobs_df.at[jobs_df.index[-1], "failedAt"] = failed[1]  
+
+        jobs_df["jobId"] = jobs_df["jobId"].str.rstrip(")").astype('int')
+        return jobs_df
+
+
+    def __find_jobs__(self, base_str: str) -> list[str]:
+        lines = base_str.splitlines()
+        arr = []  # Stores grouped sections
+        current_group = []  # Temporary storage for the current section
+
+        for line in lines:
+            if line.isupper() or not line.strip():  # New section (uppercase or empty line)
+                if current_group:  # Avoid adding empty groups
+                    arr.append(current_group)
+                current_group = [line]  # Start a new group
+            else:
+                current_group.append(line)
+
+        if current_group:  # Append the last group
+            arr.append(current_group)
+
+        # Filter out groups that do not start with an uppercase title
+        filtered_arr = [group for group in arr if group and group[0].isupper()]
+        return filtered_arr
+
+    def __clean_job_text__(self, base_str: str) -> pd.DataFrame:
+        """
+        Cleans and structures GitHub job data from CLI output.
+
+        :param base_str: Raw job text output from the GitHub CLI.
+        :return: A Pandas DataFrame with structured job data.
+        """
+        try:
+            # Remove ANSI escape sequences and unwanted characters
+            ansi_cleaned = ArqManipulation.clean_ansi_escape(base_str)
+            cleaned = ansi_cleaned.replace("✓", "PASSED |").replace("X", "FAILED |")
+
+            stripped_list = self.__find_jobs__(cleaned)
+
+            if not (x.find('JOBS') or x.find("ANNOTATIONS") for x in stripped_list):
+                return pd.DataFrame()
+
+            jobs_df = self.__build_cleaned_df__(stripped_list)
+
+            return jobs_df
+
+        except Exception as e:
+            print(f"Error processing job text: {e}")
+            return pd.DataFrame()
+   
+def str_time_to_int(time_str: str) -> int:
+    """
+    Converts a time string to seconds.
+    returns: int
+    """
+    names = ['d', 'h', 'm', 's']
+    seconds = [86400, 3600, 60, 1]
+
+    total_time = 0
+
+    for m, t in zip(names,seconds):
+        if m in time_str:
+            time_list = time_str.split(m)
+            total_time +=  int(time_list[0]) * t
+            time_str = time_list[1]
+
+    return total_time
